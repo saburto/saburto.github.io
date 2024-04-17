a15f2ebdd2687f347432a2cbdbcc30cc835e5557
Sebastian Aburto <938650+saburto@users.noreply.github.com>
2024-04-17 22:34:46 +0200

    New site (#1)
    * clean: remove all version
    
    * feat: docusaurus version

---
+++ src/pages/my-react-page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <div>
+        <h1>My React Page</h1>
+        <p>This is a React page</p>
+      </div>
+    </Layout>
+  );
+}
